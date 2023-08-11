144b7a435c7bf169e0e09ee4d436eb26ee657cf1
PriscilaNegro <priscila.luzn@hotmail.com>
2023-08-11 20:45:53 -0300

    Aula 1108 -Update, Delete e Create Pensamentos

--- controllers/ThoughtController.js
+++ controllers/ThoughtController.js
@@ -63,4 +63,48 @@ module.exports = class ThoughtController{
             res.render('thoughts/home',{thoughts, thoughtsQty, search})
         }).catch((err) => console.error(err))
     }
+
+    static createThought(req, res){
+        res.render('thoughts/create')
+    }
+
+    static createThoughtSave(req, res){
+        const thought = {
+            title:req.body.title,
+            UserId: req.session.userid
+        }
+
+        Thought.create(thought)
+        .then(()=>{
+            req.flash('message', 'Pensamento foi criado com sucesso.')
+            req.session.save(()=>{
+            res.redirect('/thoughts/dashboard')
+        })
+        })
+        .catch((eer)=> console.log(eer))
+    }
+
+    static removeThougth(req,res){
+        const id = req.body.id
+
+        Thought.destroy({where:{id:id}})
+        .then(()=> {
+            req.flash('message', 'Pensamento Excluido com sucesso.')
+            req.session.save(()=>{
+                res.redirect('/thoughts/dashboard')
+            })
+        })
+        .catch((eer)=> console.log(eer))
+    }
+
+    static updateThought(req, res){
+        const id = req.params.id
+
+        Thought.findOne({where: {id:id}, raw: true
+        .then((thought)=> {
+            res.render('thoughts/edit', {thought})
+        })
+        .catch((eer)=> console.log(eer))
+    })
+    }
 }
